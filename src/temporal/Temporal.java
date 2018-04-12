d9b4e0c30b29ac3d38bf2fe47daeeccdfc261b6c
Geovanny Flores <Geovanny Flores@192.168.1.13>
2018-04-12 10:40:17 -0600

    Este es un simple comentario sobre lo nuevo

---
+++ src/temporal/Temporal.java
@@ -0,0 +1,19 @@
+package temporal;
+
+/**
+ * Nombre de la clase: Temporal.
+ * Fecha: 12-04-2018
+ * Version: 1.0
+ * CopyRight: Geovanny Flores
+ * @author Geovanny Flores
+ */
+public class Temporal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
